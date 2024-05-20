25db4f1b89c8369dd2fda112e2c05d6332534367
catish2k <Monsk2005@gmail.com>
2024-05-20 13:44:31 +0000

    kuk

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
+const population = require('../public/json/country-by-population.json')
+const flag = require("../public/json/country-by-flag.json")
 const express = require("express");
 const app = express();
 const PORT = 3003;
 const path = require("path");
 const viewsDirectory = path.join(process.cwd(), "views");
+const fs = require('fs');
 
 //MIDDLEWARE
 app.use(express.static(path.join(__dirname, "public")));
@@ -17,3 +20,4 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
   res.render("index");
 });
+
